0686a480417c836e2f34e5d19745e608ec41c81b
dante1879 <sirenko.alex777@gmail.com>
2023-04-15 22:55:23 +0300

    task 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 // Create gallery markup
 const galleryList = document.querySelector(".gallery");
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Create gallery markup
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </li>
+        `
+    )
+    .join("");
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+var simpleLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250
+});
+
+simpleLightbox.on("show.simplelightbox", function () {
+  console.log("Image opened");
+});
